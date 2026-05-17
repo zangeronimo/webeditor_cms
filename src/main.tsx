3826031992e2425a6861aed7e931eee278f1a282
Luciano Zangeronimo <zangeronimo@gmail.com>
2026-05-17 14:21:25 -0300

    chore: add i18nProvider

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,13 @@
 import reactDom from 'react-dom/client';
 import { App } from './presentation/App';
+import { I18nProvider } from './core/i18n/presentation/i18n-provider';
+import { LocalStorage } from '@infra/http/clients/local-storage';
 
 const container = document.getElementById('root');
 const root = reactDom.createRoot(container!);
-
-root.render(<App />);
+const storage = new LocalStorage();
+root.render(
+  <I18nProvider storage={storage}>
+    <App />
+  </I18nProvider>,
+);
